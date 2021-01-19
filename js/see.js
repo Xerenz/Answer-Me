22766060ae9c819d325205b1fa0c4e2d0d1d03a7
Xerenz <martingeo15@gmail.com>
2021-01-19 11:03:17 +0530

    final changes

--- js/see.js
+++ js/see.js
@@ -8,7 +8,17 @@ $(document).ready(() => {
     .then(snap => {
         snap.forEach(doc => {
             console.log(doc.data())
-            let data = `<p><b>${doc.data().name}</b> says <i>${doc.data().question}</i></p>`
+            let data = `
+            <div class="mb-3">
+            <div class="card">
+                <p class="mt-2 ml-3">
+                    <b>${doc.data().name}</b> says <i>${doc.data().question}</i>
+                </p>
+                <div class="card-footer text-muted>
+                    ${doc.data.email}
+                </div>
+            </div>
+            </div>`
             $("#allQuestions").append(data)
         })
     })
